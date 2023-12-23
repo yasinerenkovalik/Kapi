17c8aa048d018d27f788ff81ef7c718c1c27c6a0
yasinerenkovalik <erenkovalik42@gmail.com>
2023-12-23 11:51:09 +0300

    19.12.2023

--- EncdoeGenerator.py
+++ EncdoeGenerator.py
@@ -2,39 +2,55 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import  storage
 
+cred = credentials.Certificate("kapi-c9b42-firebase-adminsdk-cejkv-f2d05ee407.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL':"https://kapi-c9b42-default-rtdb.firebaseio.com/",
+    'storageBucket':'kapi-c9b42.appspot.com'
+})
+
+
+# Importing student images
 folderPath = 'Images'
 pathList = os.listdir(folderPath)
 print(pathList)
 imgList = []
-studentIds=[]
-
+studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
-    #print(path)
-    #print(os.path.splitext(path)[0])
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+
+    # print(path)
+    # print(os.path.splitext(path)[0])
 print(studentIds)
 
 
-
-def findEncoding(imagesList):
-    encodeList=[]
+def findEncodings(imagesList):
+    encodeList = []
     for img in imagesList:
-        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encode=face_recognition.face_encodings(img)[0]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
+
     return encodeList
-print("encoding strated")
-encodeListKnow=findEncoding(imgList)
-print(encodeListKnow)
-encodeListKnowWithId=[encodeListKnow,studentIds]
-print("encoding complete")
 
-file=open("EncodeFile.p","wb")
-pickle.dump(encodeListKnowWithId,file)
+
+print("Encoding Started ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
+
+file = open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
 file.close()
-print("file saved")
-
-
-
+print("File Saved")
